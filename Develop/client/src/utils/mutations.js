3c310ab1e0b3930c12aea7f6e79bebb474045bab
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-02-05 14:50:51 -0600

    mutations completed

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,1 +1,66 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+login(email: $email, password: $password){
+token
+user {
+_id
+username
+}
+}
+}`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+addUser(username: $username, email: $email, password: $password){
+token 
+user {
+_id
+username
+}
+}
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookData: BookInput!){
+saveBook(bookData: $bookData) {
+_id
+username
+email
+bookCount
+
+savedBooks {
+bookId
+title
+description
+image
+link 
+authors
+
+}
+}
+}
+`;
+
+export const DELETE_BOOK = gql`
+mutation deleteBook($bookId: ID!){
+deleteBook(bookId: $bookId){
+_id
+username
+email
+bookCount
+
+savedBooks{
+bookId
+title
+description
+image
+link
+authors
+
+}
+}
+}
+`;
