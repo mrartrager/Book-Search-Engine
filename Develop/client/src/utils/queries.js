00ba983ee7848b1e7b15273d70ca78bb161c97a3
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-02-05 14:53:29 -0600

    queries completed

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,1 +1,22 @@
 import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+me {
+_id
+username
+email
+bookCount
+
+savedBooks{
+bookId
+title
+description
+image
+link
+authors
+
+}
+}
+}
+`;
