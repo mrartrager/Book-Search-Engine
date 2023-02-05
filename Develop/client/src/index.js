083d3b7f720f74e5969e002cb1fb488788077bb2
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-02-05 14:30:12 -0600

    switched to working in appJS instead of indexJS

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
